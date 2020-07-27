7121e8b085e62fdc6061773f28a8fd42c1c75640
abhishek-mittal <abhishekmittaloffice@gmail.com>
2020-07-27 10:29:47 +0530

    kmsk

--- _mamaearth/bin/rename.py
+++ _mamaearth/bin/rename.py
@@ -1,21 +1,31 @@
 
-# import os, re
+import os, re, random, string
+def generate_id(size=7, chars=string.ascii_uppercase + string.digits):
+        return ''.join(random.choice(chars) for _ in range(size))
 
-# def main(): 
 
-#     print('doing')
-#     for count, filename in enumerate(os.listdir('./../assets copy')):
-#         # dst = "a" + str(count) + '.png'
-#         src = './../assets copy/' + filename
-#         try:
-#             key = '.' + filename.rsplit( ".", 1 )[1] 
-#             print key
-#         except Exception as identifier:
-#             key = '.png'
+def main(): 
+
+    print('doing')
+
+    for count, filename in enumerate(os.listdir( './assets')):
+        # dst = "a" + str(count) + '.png'
+        src = './assets/' + filename
+        # print(count)
+        try:
+            key = '.' + filename.rsplit( ".", 1 )[1]
+        except Exception as identifier:
+            key = '.png'
         
-#         dst = './../assets/' + re.sub('\s|\W', '', filename ) + key
-#         # print filename.rsplit( ".", 1 )
-#         os.rename(src, dst)
+        c = re.findall('\s+', filename)
+        tt = bool(len(c))
 
-# if __name__ == '__main__':
-#     main()
+        if tt:
+            print(tt)
+            dst = './assets/' + re.sub('\s|\W', '', filename ) + generate_id() + key
+            print dst
+            os.rename(src, dst)
+
+
+if __name__ == '__main__':
+    main()
